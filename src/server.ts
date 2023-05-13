56632605288cebf20f1bd1cd05c64cc9ca32a7d1
Raj106292 <rajdas.106292@gmail.com>
2023-05-13 14:21:08 +0600

    module 6-2 done

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
-const port = 3000;
+const port = 5000;
+
+// database connection
+async function connectWithDatabase() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
+        console.log("Database connected successfully");
+    } catch(error) {
+        console.log("Database connection failed", error);
+    }
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+connectWithDatabase();
 
 app.get("/", (req, res) => {
     res.send("Hello World");
