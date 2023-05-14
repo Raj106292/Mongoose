6fa8a983e358a3395ee0408086ca6a9ccd2989dd
Raj106292 <rajdas.106292@gmail.com>
2023-05-14 17:54:14 +0600

    module 6-9 done

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,9 +1,18 @@
 import { NextFunction, Request, Response } from "express";
-import { getUserFromDB, createUserToDB } from "./user.service";
+import { getUserFromDB, createUserToDB, getOneUserByIdFromDB } from "./user.service";
 
 export const getUser = async (req: Request, res: Response) => {
    const user = await getUserFromDB();
    res.json(user);
+}
+
+export const getUserByID = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const user = await getOneUserByIdFromDB(id);
+    res.status(200).json({
+        status: "success",
+        data: user
+    })
 }
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,14 +1,19 @@
 import User from "./user.model";
 import IUser from "./user.interface";
 
+export const getUserFromDB = async () : Promise<IUser[]> => {
+    const allUser = await User.find();
+    return allUser;
+}
+
+export const getOneUserByIdFromDB = async (payload: string) : Promise<IUser | null> => {
+    const user = await User.findOne({id: payload}, {name: 1, email: 1});
+    return user;
+}
+
 export const createUserToDB = async (payload: IUser): Promise<IUser> => {
     // creating an instance by using the model
     const user = new User(payload);
     await user.save();
     return user;
-}
-
-export const getUserFromDB = async () : Promise<IUser[]> => {
-    const allUser = await User.find();
-    return allUser;
 }
